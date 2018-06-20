10a331b0d9ba3812c47ddb77d0f33398da968314
andrekorol <anrobits@yahoo.com.br>
2018-06-20 20:53:50 -0300

    Finish deepEqual implementation on deepComparison.js

--- eloquentjavascript/chapter4-data/exercises/deepComparison.js
+++ eloquentjavascript/chapter4-data/exercises/deepComparison.js
@@ -1,10 +1,26 @@
 function deepEqual (value1, value2) {
-  if (typeof value1 !== 'object' && typeof value2 !== 'object') {
+  if ((typeof value1 === 'object' && value1 !== null) &&
+      (typeof value2 === 'object' && value2 !== null)) {
+    if (Object.keys(value1).length !== Object.keys(value2).length) {
+      return false
+    } else {
+      for (let _key of Object.keys(value1)) {
+        if (value2.hasOwnProperty(_key)) {
+          return deepEqual(value1[_key], value2[_key])
+        } else {
+          return false
+        }
+      }
+    }
+  } else {
     return (value1 === value2)
-  } else if (value1 !== null && value2 !== null) {
-    
   }
 }
 
 let obj = {here: {is: 'an'}, object: 2}
-console.log(deepEqual(4, 4))
+console.log(deepEqual(obj, obj))
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}))
+// → false
+console.log(deepEqual(obj, {here: {is: 'an'}, object: 2}))
+// → true
