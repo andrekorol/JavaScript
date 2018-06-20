0e4d4cd356d682fc651b3d5b25cec316ec208e28
andrekorol <anrobits@yahoo.com.br>
2018-06-20 19:12:15 -0300

    Add deepComparison.js

---
+++ eloquentjavascript/chapter4-data/exercises/deepComparison.js
@@ -0,0 +1,10 @@
+function deepEqual (value1, value2) {
+  if (typeof value1 !== 'object' && typeof value2 !== 'object') {
+    return (value1 === value2)
+  } else if (value1 !== null && value2 !== null) {
+    
+  }
+}
+
+let obj = {here: {is: 'an'}, object: 2}
+console.log(deepEqual(4, 4))
