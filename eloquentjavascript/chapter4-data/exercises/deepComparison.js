4f2596145ab5956ba7aa0cd65785b505882c54e1
andrekorol <anrobits@yahoo.com.br>
2018-06-20 21:07:40 -0300

    Now it works on an edge case in which latter objects' properties differ

--- eloquentjavascript/chapter4-data/exercises/deepComparison.js
+++ eloquentjavascript/chapter4-data/exercises/deepComparison.js
@@ -4,12 +4,12 @@ function deepEqual (value1, value2) {
     if (Object.keys(value1).length !== Object.keys(value2).length) {
       return false
     } else {
-      for (let _key of Object.keys(value1)) {
-        if (value2.hasOwnProperty(_key)) {
-          return deepEqual(value1[_key], value2[_key])
-        } else {
-          return false
-        }
+      if (Object.keys(value1).every(function (prop) {
+        return value2.hasOwnProperty(prop)
+      })) {
+        return (Object.keys(value1).every(function (prop) {
+          return deepEqual(value1[prop], value2[prop])
+        }))
       }
     }
   } else {
@@ -24,3 +24,5 @@ console.log(deepEqual(obj, {here: 1, object: 2}))
 // → false
 console.log(deepEqual(obj, {here: {is: 'an'}, object: 2}))
 // → true
+console.log(deepEqual(obj, {here: {is: 'an'}, object: 4}))
+// → false
